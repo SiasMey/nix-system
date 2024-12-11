4c9c652fb5d215473c2fa446711b1b606c42f517
Sias Mey <siasmey@gmail.com>
2024-12-11 23:44:18 +0200

    chore: update nixos config
    Generation  Build-date           NixOS version            Kernel  Configuration Revision  Specialisation
    41 current  2024-12-11 17:16:03  25.05.20241203.55d15ad   6.6.63                          *
    
    from: foot1 GNU/Linux

--- configuration.nix
+++ configuration.nix
@@ -1,25 +1,22 @@
 # Edit this configuration file to define what should be installed on
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
-
 {
   config,
   pkgs,
-  lib,
+  options,
   ...
-}:
-
-{
+}: {
   imports = [
     # Include the results of the hardware scan.
     ./hardware-configuration.nix
   ];
+  programs.nix-ld.enable = true;
+  programs.nix-ld.libraries = options.programs.nix-ld.libraries.default;
 
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
-  # boot.initrd.kernelModules = [ "nvidia" ];
-  # boot.extraModulePackages = [ config.boot.kernelPackages.nvidia_x11 ];
 
   networking.hostName = "foot1"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
@@ -40,7 +37,7 @@
   # Enable the X11 windowing system.
   # You can disable this if you're only using the Wayland session.
   services.xserver = {
-    videoDrivers = [ "nvidia" ];
+    videoDrivers = ["nvidia"];
   };
 
   hardware.opengl.enable = true;
@@ -59,23 +56,12 @@
     enable = true;
     wayland.enable = true;
   };
-  # services.xserver.desktopManager.plasma6.enable = true;
-  # programs.river = {
-  #   enable = true;
-  #   xwayland.enable = true;
-  # };
 
   programs.hyprland = {
     enable = true;
     xwayland.enable = true;
   };
-  security.pam.services.hyprlock = { };
-
-  # Configure keymap in X11
-  # services.xserver.xkb = {
-  #   layout = "us";
-  #   variant = "";
-  # };
+  security.pam.services.hyprlock = {};
 
   # Enable CUPS to print documents.
   services.printing.enable = true;
@@ -88,16 +74,16 @@
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
 
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
+    extraConfig.pipewire."92-low-latency" = {
+      "context.properties" = {
+        "default.clock.rate" = 48000;
+        "default.clock.quantum" = 32;
+        "default.clock.min-quantum" = 32;
+        "default.clock.max-quantum" = 32;
+      };
+    };
   };
-
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.meysi = {
@@ -146,7 +132,6 @@
     curl
     kitty
     jq
-    killall
 
     dunst
     hypridle
@@ -161,22 +146,11 @@
   ];
   environment.variables.EDITOR = "nvim";
   environment.sessionVariables = {
-    # WLR_NO_HARDWARE_CURSORS = "1";
     NIXOS_OZONE_WL = "1";
   };
 
   xdg.portal.enable = true;
-  xdg.portal.extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
-
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
+  xdg.portal.extraPortals = [pkgs.xdg-desktop-portal-gtk];
 
   # Enable the OpenSSH daemon.
   services.openssh = {
@@ -188,12 +162,6 @@
     };
     openFirewall = true;
   };
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
