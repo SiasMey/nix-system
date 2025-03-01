2aa18c320953ef3de76f78f74e2c01d361f04d85
Sias Mey <siasmey@gmail.com>
2025-03-01 12:57:15 +0200

    chore: remove users from configuration for foot2

--- host/foot2/configuration.nix
+++ host/foot2/configuration.nix
@@ -24,21 +24,6 @@
     variant = "";
   };
 
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users.siasm = {
-    isNormalUser = true;
-    description = "Sias Mey";
-    extraGroups = ["networkmanager" "wheel"];
-    packages = with pkgs; [];
-  };
-
-  users.users.meysi = {
-    isNormalUser = true;
-    description = "Sias Mey";
-    extraGroups = ["networkmanager" "wheel"];
-    packages = with pkgs; [];
-  };
-
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
