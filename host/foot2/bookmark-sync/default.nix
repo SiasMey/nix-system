1e1b7a3ae8ac2ff81763ab9ee9c99b7ffdcc8837
Sias Mey <siasmey@gmail.com>
2025-03-29 10:45:51 +0200

    feat: use shiori instead of grimoire

--- host/foot2/bookmark-sync/default.nix
+++ host/foot2/bookmark-sync/default.nix
@@ -1,16 +1,6 @@
 {...}: {
-  config.virtualisation.oci-containers.containers = {
-    grimoire = {
-      image = "goniszewski/grimoire:0.4.4";
-      ports = ["0.0.0.0:5173:5173"];
-      volumes = [
-        "/var/lib/grimoire:/app/data/"
-      ];
-      environment = {
-        PORT = "5173";
-        # PUBLIC_HTTPS_ONLY=false;
-        PUBLIC_SIGNUP_DISABLED = "false";
-      };
-    };
+  services.shiori = {
+    enable = true;
+    port = 8080;
   };
 }
