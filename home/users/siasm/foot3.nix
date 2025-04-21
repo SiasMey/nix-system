ff1b60b95a680a74bd7dc7a50c8ee4d2dfe80728
Sias Mey <siasmey@gmail.com>
2025-04-21 10:57:14 +0200

    refactor: move cad module to host workload

--- home/users/siasm/foot3.nix
+++ home/users/siasm/foot3.nix
@@ -1,12 +1,12 @@
 {...}: {
   imports = [
     ../../scripts
-    ../../workloads/cad
     ../../workloads/hyprland
     ../../workloads/home.nix
     ../../workloads/editor.nix
     ../../workloads/terminal.nix
     ../../workloads/k8s.nix
     ../../workloads/devenv.nix
+    ../../workloads/bibliography.nix
   ];
 }
