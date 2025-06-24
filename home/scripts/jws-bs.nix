611d03444cf6bc7a8e196eb71a7d22d6674270cc
Sias Mey <siasmey@gmail.com>
2025-06-24 11:16:33 +0200

    fix: allow backwards and new bookmarks to be pushed

--- home/scripts/jws-bs.nix
+++ home/scripts/jws-bs.nix
@@ -5,9 +5,9 @@ pkgs.writeShellScriptBin "jws-bs" ''
   CURRENT_CHANGE=$(jj st --quiet | head -n 1)
 
   if [[ $CURRENT_CHANGE == "The working copy has no changes." ]]; then
-    jj bookmark set $WS_BOOKMARK -r@-
+    jj bookmark set $WS_BOOKMARK -r@- --allow-backwards
   else
-    jj bookmark set $WS_BOOKMARK -r@
+    jj bookmark set $WS_BOOKMARK -r@ --allow-backwards
   fi
-  jj git push
+  jj git push --allow-new
 ''
