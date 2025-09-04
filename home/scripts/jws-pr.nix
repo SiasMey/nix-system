4238d07e00556f7b7f13210f2da236e41b01f38c
Sias Mey <siasmey@gmail.com>
2025-09-04 09:00:11 +0200

    feat: add jws-pr

--- home/scripts/jws-pr.nix
+++ home/scripts/jws-pr.nix
@@ -3,9 +3,24 @@ pkgs.writeShellScriptBin "jws-pr" ''
   set -e
 
   TITLE=$1
+  SUMMARY=$2
 
-  COMMIT_SUMMARY=$(jj pr-summary)
-  PR_DESCRIPTION="$COMMIT_SUMMARY\n\nAdd ticket link"
+  TICKET=$(echo $WS_BOOKMARK | grep -oe 'uxi-[0-9]\+' | tr '[:lower:]' '[:upper:]')
 
-  gh pr create -H $WS_BOOKMARK -t $TITLE -b $PR_DESCRIPTION
+  TMPFILE=$(mktemp)
+  trap 'rm -f "$TMPFILE"' SIGTERM SIGINT EXIT
+
+  jws-bs
+  echo "## Description" > $TMPFILE
+  echo "" >> $TMPFILE
+  echo $SUMMARY >> $TMPFILE
+  echo "" >> $TMPFILE
+  jj pr-summary >> $TMPFILE
+  echo "" >> $TMPFILE
+  echo "" >> $TMPFILE
+  echo "## Related Issue(s)" >> $TMPFILE
+  echo "" >> $TMPFILE
+  echo "- <https://jira.arubanetworks.com/browse/$TICKET>" >> $TMPFILE
+
+  gh pr create -H "$WS_BOOKMARK" -t "$TITLE" -F "$TMPFILE"
 ''
