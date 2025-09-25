ffef2778ecf165f46cf389dfd3c123d360e87944
Sias Mey <siasmey@gmail.com>
2025-09-25 15:20:20 +0200

    fix: add gh cli and aws-ecr-creds helper to packages

--- home/workloads/mac/mac.nix
+++ home/workloads/mac/mac.nix
@@ -1,5 +1,4 @@
-{ pkgs, ... }:
-{
+{pkgs, ...}: {
   #podman needs to be installed via homebrew on mac
   #ghostty needs to be installed via homebrew on mac
   home.packages = [
@@ -9,6 +8,8 @@
     pkgs.awscli2
     pkgs.terraform
     pkgs.podman-compose
+    pkgs.amazon-ecr-credential-helper
+    pkgs.gh
   ];
 
   home.file = {
