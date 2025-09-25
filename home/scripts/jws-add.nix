11e50058fdf94c50a8b609c344e39eb262421ed0
Sias Mey <siasmey@gmail.com>
2025-09-25 15:20:20 +0200

    fix: add layout uv

--- home/scripts/jws-add.nix
+++ home/scripts/jws-add.nix
@@ -8,6 +8,8 @@ pkgs.writeShellScriptBin "jws-add" ''
   jj workspace add -R "$CLONE_DIR" "$PROJECT_DIR/$NAME"
   cd "$PROJECT_DIR/$NAME"
   echo "export WS_BOOKMARK=sm/$TYPE/$NAME" >> .envrc
+  echo 'export TEST_CMD="just test"' >> .envrc
+  echo "layout uv" >> .envrc
   echo "source_up" >> .envrc
   direnv allow
 ''
