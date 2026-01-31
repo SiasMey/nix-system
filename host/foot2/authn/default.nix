bee8340a24ff54b1ae1d5ec7ca4430a4fbf044c5
Sias Mey <siasmey@gmail.com>
2026-01-31 14:01:27 +0200

    fix: use path for pocket-id key

--- host/foot2/authn/default.nix
+++ host/foot2/authn/default.nix
@@ -1,6 +1,9 @@
 {...}: {
   services.pocket-id = {
     enable = true;
+    credentials = {
+      ENCRYPTION_KEY = "/var/run/pocket-id/encryption-key";
+    };
     settings = {
       APP_URL = "https://auth.dwarf-foot.dev";
       TRUST_PROXY = true;
