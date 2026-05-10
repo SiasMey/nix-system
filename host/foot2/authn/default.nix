17eeb0cf089384a16ceb83d3955a8a0dfc85e144
Sias Mey <siasmey@gmail.com>
2026-05-10 22:45:38 +0200

    fix: sort out pocket-id encryption key

--- host/foot2/authn/default.nix
+++ host/foot2/authn/default.nix
@@ -1,8 +1,14 @@
-{...}: {
+{config, ...}: {
+  age.secrets.pocket-id = {
+    file = ../../../secrets/pocket-id-key.age;
+    owner = "pocket-id";
+    group = "pocket-id";
+    mode = "600";
+  };
   services.pocket-id = {
     enable = true;
     credentials = {
-      ENCRYPTION_KEY = "/var/run/pocket-id/encryption-key";
+      ENCRYPTION_KEY = config.age.secrets.pocket-id.path;
     };
     settings = {
       APP_URL = "https://auth.dwarf-foot.dev";
